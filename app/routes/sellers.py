6b5b94754b5061cb1c91fa0598db185aa583a002
Amit Verma <vamit7631@gmail.com>
2025-01-09 15:56:09 +0530

    update seller functionality

--- app/routes/sellers.py
+++ app/routes/sellers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.services.seller_service import create_seller
+from app.services.seller_service import create_seller, get_sellers, get_seller_by_id, update_seller, delete_seller
 
 sellers_bp = Blueprint('sellers', __name__)
 
@@ -9,23 +9,23 @@ def create():
     response = create_seller(data)
     return jsonify(response), 201
 
-# @sellers_bp.route('/', methods=['GET'])
-# def read_all():
-#     response = get_sellers()
-#     return jsonify(response), 200
+@sellers_bp.route('/', methods=['GET'])
+def read_all():
+    response = get_sellers()
+    return jsonify(response), 200
 
-# @sellers_bp.route('/<string:seller_id>', methods=['GET'])
-# def read_one(seller_id):
-#     response = get_seller_by_id(seller_id)
-#     return jsonify(response), 200
+@sellers_bp.route('/<string:seller_id>', methods=['GET'])
+def read_one(seller_id):
+    response = get_seller_by_id(seller_id)
+    return jsonify(response), 200
 
-# @sellers_bp.route('/<string:seller_id>', methods=['PUT'])
-# def update(seller_id):
-#     data = request.json
-#     response = update_seller(seller_id, data)
-#     return jsonify(response), 200
+@sellers_bp.route('/<string:seller_id>', methods=['PUT'])
+def update(seller_id):
+    data = request.json
+    response = update_seller(seller_id, data)
+    return jsonify(response), 200
 
-# @sellers_bp.route('/<string:seller_id>', methods=['DELETE'])
-# def delete(seller_id):
-#     response = delete_seller(seller_id)
-#     return jsonify(response), 204
+@sellers_bp.route('/<string:seller_id>', methods=['DELETE'])
+def delete(seller_id):
+    response = delete_seller(seller_id)
+    return jsonify(response), 204
--- app/services/seller_service.py
+++ app/services/seller_service.py
@@ -9,3 +9,31 @@ def create_seller(data):
     }
     result = mongo.db.sellers.insert_one(seller)
     return {"message": "Seller created", "id": str(result.inserted_id)}
+
+def get_sellers():
+    sellers = list(mongo.db.sellers.find())
+    for seller in sellers:
+        seller["_id"] = str(seller["_id"])
+    return sellers
+
+def get_seller_by_id(seller_id):
+    seller = mongo.db.sellers.find_one({"_id": ObjectId(seller_id)})
+    if seller:
+        seller["_id"] = str(seller["_id"])
+        return seller
+    return {"error": "Seller not found"}
+
+def update_seller(seller_id, data):
+    result = mongo.db.sellers.update_one(
+        {"_id": ObjectId(seller_id)},
+        {"$set": data}
+    )
+    if result.matched_count:
+        return {"message": "Seller updated"}
+    return {"error": "Seller not found"}
+
+def delete_seller(seller_id):
+    response = mongo.db.sellers.delete_one({"_id": ObjectId(seller_id)})
+    if result.deleted_count:
+        return {"message": "Seller deleted"}
+    return {"error": "Seller not found"}
